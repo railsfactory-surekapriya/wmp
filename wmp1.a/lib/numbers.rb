ab4fd0fdb1d7b6c0b9a8df9271a523b2f0f89749
railsfactory-surekapriya <surekapriya@railsfactory.org>
2014-07-16 14:50:09 +0530

    numbers

--- wmp1.a/lib/numbers.rb
+++ wmp1.a/lib/numbers.rb
@@ -4,10 +4,10 @@ class Numbers
       num = number.to_i
         if num <20 and num>10
              "#{number.to_i}th"
-        elsif num%10 == 2
-           "#{number}nd"
-         elsif num%10 == 1
+          elsif num%10 == 1
            "#{number.to_i}st"
+             elsif num%10 == 2
+           "#{number}nd"
           elsif num%10 == 3
             "#{number.to_i}rd"
          else
