0239a19a87f180152300f1f72c0c899b2b2bc5bf
railsfactory-surekapriya <surekapriya@railsfactory.org>
2014-07-16 14:50:49 +0530

    roman to number

--- wmp1.a/lib/roman.rb
+++ wmp1.a/lib/roman.rb
@@ -1,71 +1,63 @@
 class Roman
 
-  def self.convert(num)
-c1 = 0
-c2 = 0
-c3 = 0
-c4 = 0
-c5 = 0
-c6 = 0
-c7 = 0
-c8 = 0
-c9 = 0
-number1 = num.to_i
-number = num.to_i
-while number1 >= 1000
-c1 = number1 / 1000
-number1 = number1 % 1000
+  def self.convert(input)
+       String s = ""
+         i = 0
+       i = input.to_i
+    while i >= 1000
+        s += "M"
+        i -= 1000
+    end
+    while i >= 900
+        s += "CM"
+        i -= 900
+     end
+    
+    while i >= 500
+        s += "D"
+        i -= 500
+    end
+    while i >= 400
+        s += "CD"
+        i -= 400
+    end
+    while i >= 100
+        s += "C"
+        i -= 100
+    end
+    while i >= 90
+        s += "XC"
+        i -= 90
+    end
+    while i >= 50
+        s += "L"
+        i -= 50
+    end
+    while i >= 40
+        s += "XL"
+        i -= 40
+    end
+    while i >= 10
+        s += "X"
+        i -= 10
+    end
+    while i >= 9
+        s += "IX"
+        i -= 9
+    end
+    while i >= 5
+        s += "V"
+        i -= 5
+    end
+    while i >= 4
+        s += "IV"
+        i -= 4
+    end
+    while i >= 1
+        s += "I"
+        i -= 1
+     end
+    return s
 end
-while number1 >= 500
-c2 = number1 / 500
-number1 = number1 % 500
 end
-while number1 >= 100
-c3 = number1 / 100
-number1 = number1 % 100
-end
-while number1 >= 50
-c4 = number1 / 50
-number1 = number1 % 50
-end
-while number1 >= 10
-c5 = number1 / 10
-number1 = number1 % 10
-end
-while number1 == 9
-c8 = number1 / 9
-number1 = number1 % 9
-end
-while number1 >= 5
-c6 = number1 / 5
-number1 = number1 % 5
-end
-while number1 == 4
-c9 = number1 / 4
-number1 = number1 % 4
-end
-while number1 >= 1
-c7 = number1 / 1
-number1 = number1 % 1
-end
-if number >= 1000
-"M"*c1 + "D"*c2 + "C"*c3 + "L"*c4 +"X"*c5 + "IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number >= 500 and number < 1000
-"D"*c2 + "C"*c3 + "L"*c4 +"X"*c5 + "IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number >= 100 and number < 500
-"C"*c3 + "L"*c4 +"X"*c5 + "IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number >= 50 and number < 100
-"L"*c4 +"X"*c5 + "IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number >= 10 and number < 50
-"X"*c5 + "IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number >= 5 and number < 10
-"IX"*c8 + "V"*c6 + "IV"*c9 + "I"*c7
-elsif number == 4
-"IV"*c9
-else
-"I"*c7
-end
-  end
-end
-
-
+ 
