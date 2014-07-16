c9463fe8c31f5e183b909ba239131dcde81ff1fa
railsfactory-surekapriya <surekapriya@railsfactory.org>
2014-07-16 16:13:12 +0530

    days and time

--- wmp1.a/lib/day.rb
+++ wmp1.a/lib/day.rb
@@ -6,18 +6,28 @@ require './days'
 
 get '/one' do
 month = params['month'].to_i
-day = params['day'].to_i
+date = params['date'].to_i
 year = params['year'].to_i
-one = File.read('./form1.html')
-d = ""
-if day !=0 && month !=0 && year !=0
-p = Time.new( year , month , day)
-d = p.strftime('%A')
-m = p.strftime('%B')
-a = Tamil_calendar.days1(d.to_s)
+one = File.read('./form1.html')  
+
+
+s = Roman.convert(year)
+d = Numbers.convert(date)
+spell = Spell_digit.convert(year)
+x = ""
+if month!=0 && date!=0 && year!=0
+t = Time.new(year,month,date)
+x = t.strftime('%A')
+a = Days.convert(x)
 end
-one = one + m.to_s + "\s" + Numbers.convert(day) + "\s" + year.to_s + "\s" + Roman.convert(year) + "\s" + Spell_digit.convert(year) + d + "\s" + a.to_s
+one = one + d.to_s + "\s" + month.to_s + "\s" + year.to_s + "\s" + s + "\s" + spell + "\s"
+one = one + x + "\n" + a.to_s
+
 end
+
+
+
+
 
 
   
