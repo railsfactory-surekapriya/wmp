f27b1751d928c4087ea8c3991744e2b724515ffe
railsfactory-surekapriya <surekapriya@railsfactory.org>
2014-07-16 14:43:26 +0530

    days in tamil

---
+++ wmp1.a/lib/days.rb
@@ -0,0 +1,26 @@
+class Tamil_calendar
+def self.days1 num
+   if num == "Monday"
+      return "Thingal-kizamai"
+   end
+   if num == "Tuesday"
+      return "Sevvaai-kizamai"
+   end
+   if num == "Wednesday"
+       return "Budhan-kizamai"
+   end
+   if num == "Thursday"
+     return "Vyaazha-kizamai"
+  end
+   if num == "Friday"
+      return "velli-kizamai"
+  end
+   if num == "Saturday"
+      return "sani-kizamai"
+  end
+   if num == "Sunday"
+     return "Nyaayitru_kizhamai"
+end
+end
+end
+     
