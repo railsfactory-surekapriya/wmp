8b2cefdad6cf9c739f547d0058e1ba8f3ae10022
railsfactory-surekapriya <surekapriya@railsfactory.org>
2014-07-22 11:05:36 +0530

    mysql manipulation

--- mysqltodo/sql.rb
+++ mysqltodo/sql.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 require 'sinatra'
 
 
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root') # "Database"
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root') 
 configure do
   set :views, "#{File.dirname(__FILE__)}/views"
 end
@@ -16,9 +16,9 @@ get '/home' do
 erb :home
 end
 
-get '/database' do               
+get '/show_databases' do               
 @tasks = client.query("show databases;")
-erb:show_databases
+erb :show_databases
 end   
 
 =begin
@@ -33,12 +33,19 @@ end
 get '/show_tables' do
 my_dbase = params["database"]
 client2 = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root',:database => my_dbase )
-
-
 @tables= client2.query("show tables;")
  erb :show_tables
+end
+ 
+
+get '/tables' do
+dbtable = params["table"]
+db = params["database"]
+client3 = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root',:database => db )
+@table1 = client3.query("describe #{table};")
+@table2 = client3.query("select * from #{table_name}")
+erb :describetable
 
 end
-
 
 
